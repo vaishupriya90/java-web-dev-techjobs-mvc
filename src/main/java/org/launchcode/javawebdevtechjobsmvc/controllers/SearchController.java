cde15c992cae7d880575064af2923bc14fd65dd2
vaishu <vaishupriya90@gmail.com>
2021-03-01 16:47:18 -0600

    finished the assignment except for the bonus part, added the code for search fiels i the search controler and the template, added code for the list-jobs template, refactored the code in the fragments.html file

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,17 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(@RequestParam String searchType,@RequestParam String searchTerm,Model model){
+        ArrayList<Job> jobs;
+        if(searchTerm == "" || searchTerm == "all"){
+            jobs = JobData.findAll();
+        }else {
+            jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+        }
+        model.addAttribute("jobs",jobs);
+        model.addAttribute("columns",columnChoices);
+        return "search";
+    }
 
 }
